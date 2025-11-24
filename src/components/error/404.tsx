be26a90b7383860a95ffd96ab967ef63c2b59a3d
zThundy <antonioantonelli7@gmail.com>
2025-11-24 17:18:45 +0100

    started working on routes

---
+++ src/components/error/404.tsx
@@ -0,0 +1,11 @@
+
+
+export default function NotFound() {
+    return (
+        <div>
+            404 Not Found
+            <br />
+            <a href="/">Go to Login</a>
+        </div>
+    )
+}
---
+++ src/components/login/index.tsx
@@ -0,0 +1,12 @@
+
+
+
+export default function Login() {
+    return (
+        <div>
+            Login Page<br />
+            <a href="/home">Go to Main</a><br />
+            <a href="/nibba">Go to Nibba</a>
+        </div>
+    )
+}
