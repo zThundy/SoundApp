aab09deaff65c9c968fe9be96748a82d589ad300
zThundy <antonioantonelli7@gmail.com>
2025-11-27 10:57:40 +0100

    changed router type for build process

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import style from './App.module.css'
 import theme from './mui/StyleProvider'
 
 import { ThemeProvider } from '@mui/material'
 
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { HashRouter, Routes, Route, useLocation } from 'react-router'
 
 import Login from "@/components/login"
 import Home from "@/components/home"
 import NotFound from "@/components/error/404"
 
-// <Button onClick={() => setCount((count) => count + 1)} variant="contained" color="primary">
-//   count is {count}
-// </Button>
-
-// <Button onClick={() => setCount((count) => count + 1)} disabled variant="contained" color="primary">
-//   count is {count}
-// </Button>
-
-// <Button onClick={() => setCount((count) => count + 1)} variant='outlined' color="secondary">
-//   count is {count}
-// </Button>
-
-// <Button onClick={() => setCount((count) => count + 1)} disabled variant='outlined' color="secondary">
-//   count is {count}
-// </Button>
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <ThemeProvider theme={theme}>
       <div className={style.appScroll}>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/home" element={<Home />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     </ThemeProvider>
   )
