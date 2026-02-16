e54ad15939adea9d0256b4131c065c93a0ccf752
zThundy <antonioantonelli7@gmail.com>
2026-02-16 11:34:15 +0100

    changed log to debug

--- electron/main/ipc/windowHandlers.ts
+++ electron/main/ipc/windowHandlers.ts
@@ -19,12 +19,12 @@ export async function registerWindowHandlers(getMainWindow: () => BrowserWindow 
 
   ipcMain.handle("window:is-startupopen-enabled", () => {
     const appLoginSettings = app.getLoginItemSettings()
-    console.log(appLoginSettings)
+    console.debug("[WindowHandlers]", appLoginSettings)
     return appLoginSettings.openAtLogin || false;
   })
 
   ipcMain.handle("window:set-startupopen-enabled", (_event, enabled: boolean) => {
-    console.log(`[WindowHandlers] Setting setLoginItemSettings: openAtLogin ${enabled}`)
+    console.debug(`[WindowHandlers] Setting setLoginItemSettings: openAtLogin ${enabled}`)
     app.setLoginItemSettings({
       openAtLogin: enabled,
       path: app.getPath("exe")
