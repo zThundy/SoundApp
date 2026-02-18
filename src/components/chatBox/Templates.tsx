172c74308e10097a4eb2f64cf812354d62675eeb
zThundy <antonioantonelli7@gmail.com>
2026-02-18 16:03:56 +0100

    changes to background image to templates

--- src/components/chatBox/Templates.tsx
+++ src/components/chatBox/Templates.tsx
@@ -94,8 +94,8 @@ const Templates = ({
                 <head>
                   <style>
                     * { margin: 0; padding: 0; box-sizing: border-box; }
-                    body { width: 100%; height: 100%; overflow: hidden; position: relative; }
-                    body::before { content: ''; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-image: url('./logo.png'); background-repeat: repeat; background-size: 120px 120px; opacity: 0.2; pointer-events: none; z-index: -1; }
+                    body { width: 100%; height: 100%; overflow: hidden; display: flex; flex-direction: column; justify-content: flex-end; position: relative; }
+                    body::before { content: ''; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-image: url('./logo.png'); background-repeat: repeat; background-size: 60px 60px; opacity: 0.2; pointer-events: none; z-index: -1; }
                     ${template.css}
                   </style>
                 </head>
